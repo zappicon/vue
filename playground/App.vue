04324f3871c5054f7a99cc7fe4be3492851448f9
Haroun ABIDI <harooonabidi@gmail.com>
2025-09-11 15:56:41 +0100

    ci: use pnpm with caching in GitHub Actions for faster installs

---
+++ playground/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { UnlockSimple, AddressCard, HeartSimple, UserPlus } from "../dist"
+</script>
+
+<template>
+  <main>
+    <AddressCard color="hotpink" size="40" variant="duotone" />
+
+    <UnlockSimple color="yellow" size="40" variant="duotone-line" />
+
+    <HeartSimple color="red" size="40" variant="duotone-line" />
+
+    <UserPlus color="blue" size="40" variant="duotone-line" />
+  </main>
+</template>
